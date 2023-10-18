80cf6b09edba7815cbded7e7ec70d1ff07323b15
alcaloke <35482173+alcaloke@users.noreply.github.com>
2023-10-18 01:01:17 +0000

    Update Texture.js

--- modules/Texture.js
+++ modules/Texture.js
@@ -4,14 +4,14 @@ class Texture {
   }
 
   create() {
-    this.texture = createGraphics(width / 3, height / 3, WEBGL)
+    this.texture = createGraphics(width, height, WEBGL)
     this.texture.noStroke()
     return this.texture
   }
 
   load(shader) {
     this.texture.shader(shader)
-    shader.setUniform("u_resolution", [width, height])
+    shader.setUniform("u_resolution", [canvas.width, canvas.height])
     shader.setUniform("u_time", millis() / 1000.0)
     shader.setUniform("u_mouse", [mouseX, map(mouseY, 0, height, height, 0)])
     this.texture.rect(0, 0, width, height)
